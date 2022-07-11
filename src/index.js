5ec1c274b52eca81f9e7a4ad24d1cebf4716e19d
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-11 11:43:11 +0300

    Star rating system using useState React Hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {FaStar} from "react-icons/fa";
 import reportWebVitals from './reportWebVitals';
+
+// Star rating system
+const createArray = (length) => [
+  ...Array(length)
+];
+
+function Star({selected = false, onSelect}) {
+  return (
+    <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>
+  )
+}
+
+function StarRating({totalStars = 5}) {
+  const [selectedStars, setSelectedStars] = useState(0)
+  return (
+    <>
+    {
+      createArray(totalStars).map((n, i) => (<Star key={i}  selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)}/>))};
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StarRating  totalStars={4}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
